b9f664eed38a93ef3cec22c7368b9ac415d1805f
Vatsala Kaashyap <vatsalakaashyap@gmail.com>
2024-09-30 15:31:14 +0530

    Done with Validating

--- Create/Validate/index.js
+++ Create/Validate/index.js
@@ -5,8 +5,8 @@ const { Contact, AddressBook } = require('./AddressBook.js');
 const myAddressBook = new AddressBook();
 
 try {
-    myAddressBook.addContact(new Contact('John', 'Doe', '123 Elm St', 'Springfield', 'IL', '62701', '5551234567', 'john.doe@example.com'));
-    myAddressBook.addContact(new Contact('Jane', 'Smith', '456 Oak St', 'Springfield', 'IL', '62702', '5557654321', 'jane.smith@example.com'));
+    myAddressBook.addContact(new Contact('Sohn', 'Doe', '123 Elm St', 'Springfield', 'ILhkoi', '62701', '5551234567', 'john.doe@example.com'));
+    myAddressBook.addContact(new Contact('Aane', 'Smith', '456 Oak St', 'Springfield', 'ILkhkuh', '62702', '5557654321', 'jane.smith@example.com'));
     console.log("Contacts List:", myAddressBook.listContacts());
 } catch (error) {
     console.error(error.message);
@@ -14,7 +14,7 @@ try {
 
 // Test invalid contact
 try {
-    myAddressBook.addContact(new Contact('jo', 'doe', '123', 'NY', 'NY', '1234', '555123', 'invalid-email'));
+    myAddressBook.addContact(new Contact('Konnkj', 'Gdoe', '123 Elm sr', 'NYKKJJ', 'NYJKLjhjg', '12348', '5551290009', 'invalidemail@gmail.com'));
 } catch (error) {
     console.error('Failed to add contact:', error.message);
 }
